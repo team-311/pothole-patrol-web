f6cfc65cc88f34e7d807585df4b9a2be3cc0fa3d
aurre <aurrecochearaysa@gmail.com>
2018-06-22 21:38:06 -0500

    map rendering fine

--- client/components/single-pothole/SinglePotholeView.js
+++ client/components/single-pothole/SinglePotholeView.js
@@ -1,5 +1,10 @@
-import mapboxgl from 'mapbox-gl';
 import React, { Component } from 'react';
+import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl';
+import mapAccessToken from '../../../secrets';
+
+const Map = ReactMapboxGl({
+  accessToken: mapAccessToken,
+});
 
 class SinglePothole extends Component {
   constructor() {
@@ -8,7 +13,26 @@ class SinglePothole extends Component {
   }
 
   render() {
-    return <h1>Hello</h1>;
+    return (
+      <h1>Hello</h1>,
+      (
+        <Map
+          style={'mapbox://styles/mapbox/streets-v9'}
+          containerStyle={{
+            height: '100vh',
+            width: '100vw',
+          }}
+        >
+          <Layer
+            type="symbol"
+            id="marker"
+            layout={{ 'icon-image': 'marker-15' }}
+          >
+            <Feature coordinates={[-0.481747846041145, 51.3233379650232]} />
+          </Layer>
+        </Map>
+      )
+    );
   }
 }
 
