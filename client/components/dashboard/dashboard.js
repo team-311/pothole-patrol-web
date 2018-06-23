663b4438c4bb34b85d46bfa205d4c2ac40eaa16f
Cody Heslep <codyheslep@gmail.com>
2018-06-23 12:42:01 -0500

    pulling

--- client/components/dashboard/dashboard.js
+++ client/components/dashboard/dashboard.js
@@ -1,12 +1,14 @@
 import React from 'react'
-import {PotholeListView} from '../'
+import { PotholeListView } from '../'
+import { Analytics } from '../analytics.js'
 
 const Dashboard = () => {
   return (
     <div>
+      <Analytics />
       <PotholeListView />
     </div>
-    )
+  )
 }
 
 export default Dashboard
