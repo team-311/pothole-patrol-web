df1f59962b383736a0a7c4ba32ffac97b1197fb8
Drevets <drevets@gmail.com>
2018-06-23 13:36:49 -0500

    added semantic ui for table on all orders view

--- client/components/dashboard/order-list-view.js
+++ client/components/dashboard/order-list-view.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { createGetLatestOrdersThunk } from '../../store';
-import { Header, Table, Container } from 'semantic-ui-react';
+import { Table, Button, Icon } from 'semantic-ui-react';
 import OrderRowItem from './order-row-item';
-import moment from 'moment'
+import moment from 'moment';
 
 class OrderListView extends Component {
   constructor() {
@@ -32,23 +32,58 @@ class OrderListView extends Component {
 
   render() {
     const { orders, lastPage } = this.props.orders;
-    return (
-        <Table celled padded>
-          <Table.Header>
-            <Table.Row>
-              <Table.HeaderCell>Id</Table.HeaderCell>
-              <Table.HeaderCell singleLine>Status</Table.HeaderCell>
-              <Table.HeaderCell>Date Created</Table.HeaderCell>
-              <Table.HeaderCell>Authorizer</Table.HeaderCell>
-              <Table.HeaderCell>Crew</Table.HeaderCell>
-            </Table.Row>
-          </Table.Header>
 
-          <Table.Body>
-            {orders.map(order => <OrderRowItem key={order.id} id={order.id} status={order.status} date={order.createdAt} authorizer={order.userId} crew={order.crewId}/>)}
-            <OrderRowItem />
-          </Table.Body>
-        </Table>
+    return (
+      <Table celled padded>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell textAlign="center">Id</Table.HeaderCell>
+            <Table.HeaderCell singleLine textAlign="center">
+              Status
+            </Table.HeaderCell>
+            <Table.HeaderCell textAlign="center">Date Created</Table.HeaderCell>
+            <Table.HeaderCell textAlign="center">Authorizer</Table.HeaderCell>
+            <Table.HeaderCell textAlign="center">Crew</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+
+        <Table.Body>
+          {orders.map(order => {
+            const formattedDate = moment(order.createdAt).format(
+              'dddd MMMM D Y'
+            );
+            const [day, month, dayNumber, year] = formattedDate.split(' ');
+            const date = [month, ' ', dayNumber, ', ', year].join('');
+            return (
+              <OrderRowItem
+                key={order.id}
+                id={order.id}
+                status={order.status}
+                date={date}
+                authorizer={order.userId}
+                crew={order.crewId}
+              />
+            );
+          })}
+        </Table.Body>
+        <Table.Footer fullWidth>
+          <Table.Row>
+            <Table.HeaderCell />
+            <Table.Cell textAlign='left' colSpan="4">
+            Viewing {this.state.page} out of {lastPage}
+              <Button
+                floated="right"
+                labelPosition="left"
+                primary
+                size="large"
+                onClick={this.handleClick}
+              >
+              View More
+              </Button>
+            </Table.Cell>
+          </Table.Row>
+        </Table.Footer>
+      </Table>
     );
   }
 }
--- client/components/dashboard/order-row-item.js
+++ client/components/dashboard/order-row-item.js
@@ -4,20 +4,21 @@ import { createGetLatestOrdersThunk } from '../../store';
 import { Header, Table, Rating } from 'semantic-ui-react';
 
 const OrderRowItem = props => (
-  <Table.Row>
-    <Table.Cell singleLine>{props.id}</Table.Cell>
+
+  <Table.Row >
+    <Table.Cell singleLine textAlign="center">{props.id}</Table.Cell>
     <Table.Cell>
       <Header as="h4" textAlign="center">
         {props.status}
       </Header>
     </Table.Cell>
-    <Table.Cell textAlign="right">
+    <Table.Cell textAlign="center">
       <a href="#">{props.date}</a>
     </Table.Cell>
-    <Table.Cell>
+    <Table.Cell textAlign="center">
       {props.authorizer}
-    </Table.Cell>
-    <Table.Cell>
+    </Table.Cell >
+    <Table.Cell textAlign="center">
       {props.crew}
     </Table.Cell>
   </Table.Row>
