bdf3c8479d86873dc7f2cd98c2b3508bf13996d8
aurre <aurrecochearaysa@gmail.com>
2018-06-23 18:22:00 -0500

    solving conflicts

--- client/routes.js
+++ client/routes.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter, Route, Switch } from 'react-router-dom';
@@ -9,61 +8,36 @@ import {
   UserHome,
   Dashboard,
   SinglePothole,
+  Analytics,
 } from './components';
 import { me } from './store';
-=======
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { withRouter, Route, Switch } from 'react-router-dom'
-import PropTypes from 'prop-types'
-import { Login, Signup, UserHome, Dashboard, Analytics } from './components'
-import { me } from './store'
->>>>>>> 4100ed42a867e5ca7643c6fb568a9de4ac3e3ec9
 
 /**
  * COMPONENT
  */
 class Routes extends Component {
   componentDidMount() {
-<<<<<<< HEAD
     this.props.loadInitialData();
   }
 
   render() {
     const { isLoggedIn } = this.props;
-=======
-    this.props.loadInitialData()
-  }
-
-  render() {
-    const { isLoggedIn } = this.props
->>>>>>> 4100ed42a867e5ca7643c6fb568a9de4ac3e3ec9
 
     return (
       <Switch>
         {/* Routes placed here are available to all visitors */}
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
-<<<<<<< HEAD
-
         {isLoggedIn && (
-=======
-        {
-          isLoggedIn &&
->>>>>>> 4100ed42a867e5ca7643c6fb568a9de4ac3e3ec9
           <Switch>
             {/* Routes placed here are only available after logging in */}
             <Route path="/home" component={UserHome} />
             <Route path="/dashboard" component={Dashboard} />
-<<<<<<< HEAD
             <Route path="/singlepothole/:id" component={SinglePothole} />
-          </Switch>
-        )}
-=======
             <Route path="/analytics" component={Analytics} />
           </Switch>
-        }
->>>>>>> 4100ed42a867e5ca7643c6fb568a9de4ac3e3ec9
+        )}
+
         {/* Displays our Login component as a fallback */}
         <Route component={Login} />
       </Switch>
@@ -85,17 +59,10 @@ const mapState = state => {
 const mapDispatch = dispatch => {
   return {
     loadInitialData() {
-<<<<<<< HEAD
       dispatch(me());
     },
   };
 };
-=======
-      dispatch(me())
-    }
-  }
-}
->>>>>>> 4100ed42a867e5ca7643c6fb568a9de4ac3e3ec9
 
 // The `withRouter` wrapper makes sure that updates are not blocked
 // when the url changes
