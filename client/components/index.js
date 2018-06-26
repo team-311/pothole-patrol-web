e1f3cdf639543ce33ea49d2bab9b252d2a26a4ef
Cody Heslep <codyheslep@gmail.com>
2018-06-26 11:32:04 -0500

    pulling

--- client/components/index.js
+++ client/components/index.js
@@ -10,18 +10,12 @@ export { Login, Signup } from './auth-form';
 export { default as PotholeListView } from './dashboard/pothole-list-view';
 export { default as Dashboard } from './dashboard/dashboard';
 export { default as SinglePothole } from './single-pothole/SinglePotholeView';
-<<<<<<< HEAD
 export { Analytics } from './analytics/analytics';
 export { AvgTimeComplete } from './analytics/avg-time-complete'
 export { ReportedPerDay } from './analytics/reported-per-day'
 export { default as OrderListCard } from './dashboard/order-list-card'
 export { default as OrderRowItem } from './dashboard/order-row-item'
 export { default as OrderListView } from './order-list-view'
-=======
-export { Analytics } from './analytics';
-export {default as OrderListCard} from './dashboard/order-list-card'
-export {default as OrderRowItem} from './dashboard/order-row-item'
-export {default as OrderListView} from './order-list-view'
-export {default as SingleOrderView} from './single-order/SingleOrderView'
-export {default as PotholeRow} from './single-order/PotholeRow'
->>>>>>> a36054a72382b96a85a73628437ea871aa509e25
+export { default as SingleOrderView } from './single-order/SingleOrderView'
+export { default as AllPotholeView } from './all-pothole/all-pothole'
+export { default as PotholeRow } from './single-order/PotholeRow'
