371d20b46719eddd37ac7b59d0d63b55964f02d9
chawes <hawes.conor@gmail.com>
2018-06-25 20:07:54 -0500

    Adding middleware for routes

---
+++ server/api/middleware.js
@@ -0,0 +1,35 @@
+const createError = (message, code) => {
+  const error = new Error(message)
+  error.status = code || 500
+  return error
+}
+
+const isResident = (req, res, next) => {
+  if (req.user && req.user.type === 'resident') {
+    next()
+  } else {
+    next(createError('Not Authorized', 401))
+  }
+}
+
+const isCrewOrAdmin = (req, res, next) => {
+  if (req.user && (req.user.type === 'admin' || req.user.type === 'crew' )) {
+    next()
+  } else {
+    next(createError('Not Authorized', 401))
+  }
+}
+
+const isAdmin = (req, res, next) => {
+  if (req.user && req.user.type === 'admin') {
+    next()
+  } else {
+    next(createError('Not Authorized', 401))
+  }
+}
+
+module.exports = {
+  isResident,
+  isCrewOrAdmin,
+  isAdmin,
+}
