882f9cd7801ae2a09b9751104f176524b2ca2bfe
aurre <aurrecochearaysa@gmail.com>
2018-06-27 13:54:10 -0500

    upVotes column added

--- server/db/models/pothole.js
+++ server/db/models/pothole.js
@@ -1,118 +1,188 @@
-const Sequelize = require('sequelize')
-const Op = Sequelize.Op
-const db = require('../db')
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
+const db = require('../db');
 
-const Pothole = db.define('pothole', {
-  imageUrl: {
-    type: Sequelize.STRING
+const Pothole = db.define(
+  'pothole',
+  {
+    imageUrl: {
+      type: Sequelize.STRING,
+    },
+    description: {
+      type: Sequelize.TEXT,
+    },
+    placement: {
+      type: Sequelize.ENUM(
+        'Bike Lane',
+        'Crosswalk',
+        'Curb Lane',
+        'Intersection',
+        'Traffic Lane'
+      ),
+    },
+    completionDate: {
+      type: Sequelize.DATE,
+    },
+    status: {
+      type: Sequelize.STRING,
+      defaultValue: 'Open',
+    },
+    serviceNumber: {
+      type: Sequelize.STRING,
+    },
+    ward: {
+      type: Sequelize.INTEGER,
+    },
+    latitude: {
+      type: Sequelize.DECIMAL(18, 15),
+    },
+    longitude: {
+      type: Sequelize.DECIMAL(18, 15),
+    },
+    mostRecentAction: {
+      type: Sequelize.STRING,
+    },
+    subscribed: {
+      type: Sequelize.BOOLEAN,
+    },
+    streetAddress: {
+      type: Sequelize.STRING,
+    },
+    zip: {
+      type: Sequelize.STRING(5),
+    },
+    updatedAt: {
+      type: Sequelize.DATE,
+      defaultValue: Sequelize.literal('NOW()'),
+    },
+    location: {
+      type: Sequelize.GEOMETRY('POINT'),
+    },
+    upVotes: {
+      type: Sequelize.INTEGER,
+      defaultValue: 1,
+    },
+    priority: {
+      type: Sequelize.VIRTUAL,
+      get() {
+        return this.getDataValue('id') / 100;
+      },
+    },
   },
-  description: {
-    type: Sequelize.TEXT
-  },
-  placement: {
-    type: Sequelize.ENUM('Bike Lane', 'Crosswalk', 'Curb Lane', 'Intersection', 'Traffic Lane')
-  },
-  completionDate: {
-    type: Sequelize.DATE
-  },
-  status: {
-    type: Sequelize.STRING,
-    defaultValue: 'Open'
-  },
-  serviceNumber: {
-    type: Sequelize.STRING
-  },
-  ward: {
-    type: Sequelize.INTEGER
-  },
-  latitude: {
-    type: Sequelize.DECIMAL(18, 15)
-  },
-  longitude: {
-    type: Sequelize.DECIMAL(18, 15)
-  },
-  mostRecentAction: {
-    type: Sequelize.STRING
-  },
-  subscribed: {
-    type: Sequelize.BOOLEAN
-  },
-  streetAddress: {
-    type: Sequelize.STRING,
-  },
-  zip: {
-    type: Sequelize.STRING(5),
-  },
-  updatedAt: {
-    type: Sequelize.DATE,
-    defaultValue: Sequelize.literal('NOW()'),
-  },
-  location: {
-    type: Sequelize.GEOMETRY('POINT')
-  },
-  priority: {
-    type: Sequelize.VIRTUAL,
-    get() {
-      return this.getDataValue('id') / 100
-    }
+  {
+    indexes: [
+      {
+        name: 'nearby_potholes',
+        fields: ['status', 'latitude', 'longitude'],
+        where: {
+          status: 'Open',
+        },
+      },
+    ],
   }
-}, {
-  indexes: [
-    {
-      name: 'nearby_potholes',
-      fields: ['status', 'latitude', 'longitude'],
-      where: {
-        status: 'Open'
-      }
-    }
-  ]
-});
+);
 
 // class methods
-Pothole.findNearby = function (lat, lon, radius = process.env.NEARBY_POTHOLE_RADIUS) {
-  const location = Sequelize.literal(`ST_GeomFromText('POINT(${lon} ${lat})')`)
-  const distance = Sequelize.fn('ST_Distance_Sphere', Sequelize.col('location'), location)
+Pothole.findNearby = function(
+  lat,
+  lon,
+  radius = process.env.NEARBY_POTHOLE_RADIUS
+) {
+  const location = Sequelize.literal(`ST_GeomFromText('POINT(${lon} ${lat})')`);
+  const distance = Sequelize.fn(
+    'ST_Distance_Sphere',
+    Sequelize.col('location'),
+    location
+  );
 
   return Pothole.findAll({
-    attributes: ['id', 'priority', 'placement', 'latitude', 'longitude', 'streetAddress', 'status', 'serviceNumber', [distance, 'distance']],
+    attributes: [
+      'id',
+      'priority',
+      'placement',
+      'latitude',
+      'longitude',
+      'streetAddress',
+      'status',
+      'serviceNumber',
+      [distance, 'distance'],
+    ],
     order: [[distance, 'ASC']],
-    where: [{status: 'Open'}, Sequelize.where(distance, { [Op.lte]: radius })],
+    where: [
+      { status: 'Open' },
+      Sequelize.where(distance, { [Op.lte]: radius }),
+    ],
     limit: 25,
-  })
-}
+  });
+};
 
-Pothole.getNext = function (lat = '41.895266', lon = '-87.639035', radius = process.env.NEXT_POTHOLE_RADIUS) {
-  const location = Sequelize.literal(`ST_GeomFromText('POINT(${lon} ${lat})')`)
-  const distance = Sequelize.fn('ST_Distance_Sphere', Sequelize.col('location'), location)
+Pothole.getNext = function(
+  lat = '41.895266',
+  lon = '-87.639035',
+  radius = process.env.NEXT_POTHOLE_RADIUS
+) {
+  const location = Sequelize.literal(`ST_GeomFromText('POINT(${lon} ${lat})')`);
+  const distance = Sequelize.fn(
+    'ST_Distance_Sphere',
+    Sequelize.col('location'),
+    location
+  );
 
   return Pothole.findAll({
-    attributes: ['id', 'priority', 'placement', 'latitude', 'longitude', 'streetAddress', 'status', 'serviceNumber', 'completionDate', [distance, 'distance']],
+    attributes: [
+      'id',
+      'priority',
+      'placement',
+      'latitude',
+      'longitude',
+      'streetAddress',
+      'status',
+      'serviceNumber',
+      'completionDate',
+      [distance, 'distance'],
+    ],
     order: [[distance, 'ASC']],
-    where: [{status: 'Open', reporterId: null}, Sequelize.where(distance, { [Op.lte]: radius })],
+    where: [
+      { status: 'Open', reporterId: null },
+      Sequelize.where(distance, { [Op.lte]: radius }),
+    ],
     limit: 25,
-  })
-    .then((potholes) => {
-      const highestPriority = potholes.sort((a, b) => b.priority - a.priority)[0]
-      return highestPriority
-    })
-}
+  }).then(potholes => {
+    const highestPriority = potholes.sort((a, b) => b.priority - a.priority)[0];
+    return highestPriority;
+  });
+};
 
-Pothole.getClosest = function (lat = '41.895266', lon = '-87.639035') {
-  const location = Sequelize.literal(`ST_GeomFromText('POINT(${lon} ${lat})')`)
-  const distance = Sequelize.fn('ST_Distance_Sphere', Sequelize.col('location'), location)
+Pothole.getClosest = function(lat = '41.895266', lon = '-87.639035') {
+  const location = Sequelize.literal(`ST_GeomFromText('POINT(${lon} ${lat})')`);
+  const distance = Sequelize.fn(
+    'ST_Distance_Sphere',
+    Sequelize.col('location'),
+    location
+  );
 
   return Pothole.findAll({
-    attributes: ['id', 'priority', 'placement', 'latitude', 'longitude', 'status', 'streetAddress', 'serviceNumber', 'completionDate', [distance, 'distance']],
+    attributes: [
+      'id',
+      'priority',
+      'placement',
+      'latitude',
+      'longitude',
+      'status',
+      'streetAddress',
+      'serviceNumber',
+      'completionDate',
+      [distance, 'distance'],
+    ],
     order: [[distance, 'ASC']],
     where: {
       status: 'Open',
       reporterId: null,
     },
     limit: 25,
-  })
-    .then((potholes) => {
-      return potholes[0]
-    })
-}
+  }).then(potholes => {
+    return potholes[0];
+  });
+};
 
-module.exports = Pothole
+module.exports = Pothole;
